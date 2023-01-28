0275885b034ed3fcc14b520ebbed53b39be611f3
Aissam Ahbar <aissam.ahbar@gmail.com>
2023-01-28 18:15:53 +0100

    add stream

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 const fs = require('fs');
 
-global.mapIdNames = {};
-global.map = {};
+let mapIdNames = {};
+let map = {};
 
 const CHUNK_SIZE = 10000000; // 10MB
 
@@ -19,8 +19,7 @@ async function readStreamFile(filename) {
 
 async function main() {
   let res1 = await readStreamFile('./data1.json');
-  loadNames(res1);
-  console.log(global.mapIdNames)
+  await loadNames(res1);
 
   //let res2 = await readStreamFile('./data2.json');
   //console.log("res2", JSON.parse(res2).length)
@@ -29,17 +28,18 @@ async function main() {
 
 main();
 
-function loadNames(json) {
+async function loadNames(json) {
   try {
     const items = json;
     for (let item of items) {
       if (item && item.id && item.name) {
-        global.mapIdNames[item.id] = {
+        mapIdNames[item.id] = {
           name: item.name,
           brand: item.brand,
         };
       }
     }
+    console.log(mapIdNames)
   } catch (err) {
     console.log('Error parsing JSON string:', err);
   }
@@ -49,22 +49,22 @@ function loadData(jsonString) {
   try {
     const items = JSON.parse(jsonString);
     for (let item of items) {
-      if (global.mapIdNames[item.fields.id]) {
-        item.fields['name'] = global.mapIdNames[item.fields.id].name;
-        item.fields['brand'] = global.mapIdNames[item.fields.id].brand;
+      if (mapIdNames[item.fields.id]) {
+        item.fields['name'] = mapIdNames[item.fields.id].name;
+        item.fields['brand'] = mapIdNames[item.fields.id].brand;
       }
-      if (!global.map[item.fields.ville.toLowerCase()]) {
-        global.map[item.fields.ville.toLowerCase()] = [];
-        global.map[item.fields.ville.toLowerCase()].push(item.fields);
+      if (!map[item.fields.ville.toLowerCase()]) {
+        map[item.fields.ville.toLowerCase()] = [];
+        map[item.fields.ville.toLowerCase()].push(item.fields);
       } else {
-        global.map[item.fields.ville.toLowerCase()].push(item.fields);
+        map[item.fields.ville.toLowerCase()].push(item.fields);
       }
     }
   } catch (err) {
     console.log('Error parsing JSON string:', err);
   }
-  for (let city of Object.keys(global.map)) {
-    const jsonString = JSON.stringify(global.map[city]);
+  for (let city of Object.keys(map)) {
+    const jsonString = JSON.stringify(map[city]);
     fs.writeFile('./' + city + '.json', jsonString, (err) => {
       if (err) {
         console.log('Error writing file', err);
