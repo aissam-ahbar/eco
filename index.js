3f8707c0d7b33bab32c54101a2cfc0585d2d8ec0
Aissam Ahbar <aissam.ahbar@gmail.com>
2023-01-28 18:13:37 +0100

    add stream

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function readStreamFile(filename) {
 async function main() {
   let res1 = await readStreamFile('./data1.json');
   loadNames(res1);
-  console.log(mapIdNames)
+  console.log(global.mapIdNames)
 
   //let res2 = await readStreamFile('./data2.json');
   //console.log("res2", JSON.parse(res2).length)
@@ -34,7 +34,7 @@ function loadNames(json) {
     const items = json;
     for (let item of items) {
       if (item && item.id && item.name) {
-        mapIdNames[item.id] = {
+        global.mapIdNames[item.id] = {
           name: item.name,
           brand: item.brand,
         };
@@ -49,22 +49,22 @@ function loadData(jsonString) {
   try {
     const items = JSON.parse(jsonString);
     for (let item of items) {
-      if (mapIdNames[item.fields.id]) {
-        item.fields['name'] = mapIdNames[item.fields.id].name;
-        item.fields['brand'] = mapIdNames[item.fields.id].brand;
+      if (global.mapIdNames[item.fields.id]) {
+        item.fields['name'] = global.mapIdNames[item.fields.id].name;
+        item.fields['brand'] = global.mapIdNames[item.fields.id].brand;
       }
-      if (!map[item.fields.ville.toLowerCase()]) {
-        map[item.fields.ville.toLowerCase()] = [];
-        map[item.fields.ville.toLowerCase()].push(item.fields);
+      if (!global.map[item.fields.ville.toLowerCase()]) {
+        global.map[item.fields.ville.toLowerCase()] = [];
+        global.map[item.fields.ville.toLowerCase()].push(item.fields);
       } else {
-        map[item.fields.ville.toLowerCase()].push(item.fields);
+        global.map[item.fields.ville.toLowerCase()].push(item.fields);
       }
     }
   } catch (err) {
     console.log('Error parsing JSON string:', err);
   }
-  for (let city of Object.keys(map)) {
-    const jsonString = JSON.stringify(map[city]);
+  for (let city of Object.keys(global.map)) {
+    const jsonString = JSON.stringify(global.map[city]);
     fs.writeFile('./' + city + '.json', jsonString, (err) => {
       if (err) {
         console.log('Error writing file', err);
